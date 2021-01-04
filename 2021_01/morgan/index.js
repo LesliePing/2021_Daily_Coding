1e618beffa877ca3a64f39dda9f6255ff641a1ee
LesliePing <894324831@qq.com>
2021-01-04 09:18:31 +0800

    处理post请求

--- 2021_01/morgan/index.js
+++ 2021_01/morgan/index.js
@@ -2,9 +2,10 @@ var express = require('express');
 var logger = require('morgan');
 var fs = require('fs');
 var FileStreamRotator = require('file-stream-rotator');
-
+// 处理post请求，需要body-parser
+var bodyParser = require('body-parser')
 var app = new express();
-
+app.use(bodyParser.json({ limit: '50mb' }));
 //设置日志文件目录
 var logDirectory = __dirname + '/logs';
 //确保日志文件目录存在 没有则创建
@@ -22,5 +23,12 @@ app.use(logger('combined', { stream: accessLogStream }));//写入日志文件
 app.get('/', function (req, res) {
   res.send('hello world');
 });
+
+app.post('/testPost', function (req, res) {
+  res.send('hello world');
+  console.log('req params', req.body)
+});
+
+
 console.log('Server Listening On Port: 3000')
 app.listen(3000);
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### 日志
 - 2021_01/consolewords/  打印台进度条
 - 2021_01/morgan         express的http请求日志中间件
+- 2020_01/morgan 引入body-parser 格式化post请求
