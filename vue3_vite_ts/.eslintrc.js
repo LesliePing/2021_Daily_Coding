b19253b94301db8b043826699d7ebad51bee0ddb
LesliePing <894324831@qq.com>
2021-01-08 10:26:21 +0800

    更新依赖，ts支持

--- vue3_vite_ts/.eslintrc.js
+++ vue3_vite_ts/.eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
     "vue/no-unused-components": "off",
     'vue/no-unused-vars': 'off',
+    'vue/no-multiple-template-root': 'off',
     'vue/no-v-for-template-key-on-child': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/camelcase': 'off',
